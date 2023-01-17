9fc82a1e942151cd765485d3f6e7fabf92e54d04
EmmanuelMmanda <luneya17@gmail.com>
2023-01-17 09:45:33 +0300

    api for caluation area, perimeter and circumference

---
+++ main.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+PI = 3.14
+
+@app.route('/')
+def index():
+    return '<h1 style="text-align: center; color: royalblue"> Area And Perimeter API<h1/>'
+
+@app.route('/api/calc/area/circle/')
+def calc_Area_Of_Circle():
+    radius = int(request.argss.get('radius'))
+    area = (PI * (radius * radius))
+    response =  {"radius": radius, "area": area }
+    return jsonify(response)
+
+@app.route('/api/calc/area/rectangle/')
+def calc_Area_Of_Rectangle():
+    width = int(request.args.get('width'))
+    height = int(request.args.get('height'))
+    area = width * height
+    response = {"width": width, "height": height, "area": area}
+    return jsonify(response)
+
+@app.route('/api/calc/area/triangle/')
+def calc_Area_Of_Triangle():
+    base = int(request.args.get('base'))
+    height = int(request.args.get('height'))
+    area = 1/2 *base * height
+    response = {"base": base, "height": height, "area": area}
+    return jsonify(response)
+
+@app.route('/api/calc/perimeter/rectangle/')
+def calc_Perimeter_Of_Rectangle():
+    width = int(request.args.get('width'))
+    height = int(request.args.get('height'))
+    perimeter = (2*height) + (2*width)
+    response = {"width": width, "height": height, "perimeter": perimeter}
+    return jsonify(response)
+
+@app.route('/api/calc/perimeter/triangle/')
+def  calc_Perimeter_Of_Triangle():
+    side1 = int(request.args.get('side1'))
+    side2 = int(request.args.get('side2'))
+    side3 = int(request.args.get('side3'))
+    perimeter = side1 + side2 + side3
+    response = {"side1": side1, "side2": side2, "side3": side3, "perimeter": perimeter}
+    return jsonify(response)
+
+
+@app.route('/api/calc/circumference/circle/')
+def  calc_Circumference_Of_Circle():
+    if request.args.get('radius'):
+        radius = int(request.args.get('radius'))
+        circumference =  2 * PI * radius
+        response = {"radius": radius, "circumference": circumference}
+    elif request.args.get('diameter'):
+        diameter = int(request.args.get('diameter'))
+        circumference =  PI * diameter
+        response = {"diameter": diameter, "circumference": circumference}
+
+    return jsonify(response)
+
+
+
+
+
+
+
+
+
+
+
+
+app.run(host='0.0.0.0', port=81)
